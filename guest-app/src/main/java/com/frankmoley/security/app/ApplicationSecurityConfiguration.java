9a87c664ecf1e2a8ec7cc37e1c964e0511239649
bessam <bsahli@wissal-sys.com>
2019-11-15 18:42:58 +0100

    Added authorization to the basic form authentication

--- guest-app/src/main/java/com/frankmoley/security/app/ApplicationSecurityConfiguration.java
+++ guest-app/src/main/java/com/frankmoley/security/app/ApplicationSecurityConfiguration.java
@@ -6,14 +6,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) //turn on method level security to work with roles (@Secure & @PreAuthorise in the controllers).
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private MyUserDetailsService userDetailsService;
@@ -22,8 +26,23 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(new BCryptPasswordEncoder(11));
+        provider.setAuthoritiesMapper(authoritiesMapper());
+
+        // TODO: 15/11/2019 replace the noopPasswordEncoder with bcrypt
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
         return provider;
+    }
+
+    /**
+     * to map the roles in the DB with the roles in the app (it adds Role in front of ADMIN or USER for example).
+     * @return
+     */
+    @Bean
+    public GrantedAuthoritiesMapper authoritiesMapper(){
+        SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
+        authorityMapper.setConvertToUpperCase(true);
+        authorityMapper.setDefaultAuthority("USER");
+        return authorityMapper;
     }
 
     @Override
--- guest-app/src/main/java/com/frankmoley/security/app/auth/MyUserDetailsService.java
+++ guest-app/src/main/java/com/frankmoley/security/app/auth/MyUserDetailsService.java
@@ -5,24 +5,28 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final AuthGroupRepository authGroupRepository;
 
-    public MyUserDetailsService(UserRepository userRepository){
+    public MyUserDetailsService(UserRepository userRepository, AuthGroupRepository authGroupRepository){
         super();
         this.userRepository = userRepository;
+        this.authGroupRepository = authGroupRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         User user = this.userRepository.findByUsername(s);
-        System.out.println("User "+user.getUsername());
+        List<AuthGroup> authGroups = this.authGroupRepository.findByUsername(s);
         if (null == user){
             System.out.println("Cannot find username "+s);
             throw new UsernameNotFoundException("Cannot find User name "+s);
         }
-        return new MyUserPrincipal(user);
+        return new MyUserPrincipal(user, authGroups);
     }
 }
