a3616955c194cd6bb9c992b427698951a8fcc560
bessam <bsahli@wissal-sys.com>
2019-11-16 11:20:06 +0100

    FORM-BASED AUTHENTICATION WITH DB AND ROLES

--- guest-app/src/main/java/com/frankmoley/security/app/ApplicationSecurityConfiguration.java
+++ guest-app/src/main/java/com/frankmoley/security/app/ApplicationSecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMap
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * this is a basic authentication configuration with a authorisation management
@@ -63,7 +64,15 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
                 .antMatchers("/", "/index", "/css/*", "/js/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .httpBasic();
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .and()
+                .logout().invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/logout-success").permitAll();
+
+        ;
     }
 
 }
--- guest-app/src/main/java/com/frankmoley/security/app/GuestController.java
+++ guest-app/src/main/java/com/frankmoley/security/app/GuestController.java
@@ -23,51 +23,61 @@ public class GuestController {
 
     private final GuestService guestService;
 
-    public GuestController(GuestService guestService){
+    public GuestController(GuestService guestService) {
         super();
         this.guestService = guestService;
     }
 
-    @GetMapping(value={"/", "/index"})
-    public String getHomePage(Model model){
+    @GetMapping(value = {"/", "/index"})
+    public String getHomePage(Model model) {
 
         return "index";
     }
 
+    @GetMapping(value = "/login")
+    public String getLoginPage(Model model) {
+        return "login";
+    }
+
+    @GetMapping(value = "/logout-success")
+    public String getLogoutPage(Model model) {
+        return "logout";
+    }
+
+    @GetMapping(value = "/guests")
     @PreAuthorize("hasRole('ROLE_USER')")
-    @GetMapping(value="/guests")
-    public String getGuests(Model model){
+    public String getGuests(Model model) {
         List<Guest> guests = this.guestService.getAllGuests();
         model.addAttribute("guests", guests);
         return "guests-view";
     }
 
-    @GetMapping(value="/guests/add")
+    @GetMapping(value = "/guests/add")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public String getAddGuestForm(Model model){
+    public String getAddGuestForm(Model model) {
         return "guest-view";
     }
 
-    @PostMapping(value="/guests")
+    @PostMapping(value = "/guests")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ModelAndView addGuest(HttpServletRequest request, Model model, @ModelAttribute GuestModel guestModel){
+    public ModelAndView addGuest(HttpServletRequest request, Model model, @ModelAttribute GuestModel guestModel) {
         Guest guest = this.guestService.addGuest(guestModel);
         model.addAttribute("guest", guest);
         request.setAttribute(View.RESPONSE_STATUS_ATTRIBUTE, HttpStatus.TEMPORARY_REDIRECT);
         return new ModelAndView("redirect:/guests/" + guest.getId());
     }
 
-    @GetMapping(value="/guests/{id}")
+    @GetMapping(value = "/guests/{id}")
     @PreAuthorize("hasRole('ROLE_USER')")
-    public String getGuest(Model model, @PathVariable long id){
+    public String getGuest(Model model, @PathVariable long id) {
         Guest guest = this.guestService.getGuest(id);
         model.addAttribute("guest", guest);
         return "guest-view";
     }
 
-    @PostMapping(value="/guests/{id}")
+    @PostMapping(value = "/guests/{id}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public String updateGuest(Model model, @PathVariable long id, @ModelAttribute GuestModel guestModel){
+    public String updateGuest(Model model, @PathVariable long id, @ModelAttribute GuestModel guestModel) {
         Guest guest = this.guestService.updateGuest(id, guestModel);
         model.addAttribute("guest", guest);
         model.addAttribute("guestModel", new GuestModel());
